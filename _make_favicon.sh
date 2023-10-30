07913a7ee773c70305f2b9dc036a3de4877b37f3
王少聪 <wangshaocong@wangshaocongdeMacBook-Pro.local>
2023-10-31 00:17:05 +0800

    update

--- _make_favicon.sh
+++ _make_favicon.sh
@@ -1,3 +1,4 @@
 # From https://github.com/leonidk/new_website
 # convert -background transparent "images/circle_crop.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
-convert -background transparent "images/icon-tree.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+# convert -background transparent "images/icon-tree.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
+convert -background transparent "images/autumn-tree.png" -define icon:auto-resize=16,24,32,48,64,72,96,128,256 "favicon.ico"
